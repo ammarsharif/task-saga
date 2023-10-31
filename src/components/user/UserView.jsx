4010b21b8857467d8b7351da7340d98fd968814b
ammarsharif <ammarsharif377@gmail.com>
2023-10-31 18:18:34 +0500

    Small Fixes

--- src/components/user/UserView.jsx
+++ src/components/user/UserView.jsx
@@ -4,14 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 const UserView = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.UserData.data);
-  console.log(users);
   useEffect(() => {
     dispatch({ type: 'UserData/fetchUsers' });
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>UserView</h1>
+    <div style={{ margin: '1em' }}>
+      <h1 style={{ marginLeft: '0.5em' }}>User Data</h1>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
